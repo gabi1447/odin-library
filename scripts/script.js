00b2ae559a6c5c6b963f40a4393f6ea9f563984e
Gabriel <gabrielpachecotec@gmail.com>
2024-10-04 06:46:30 +0100

    Finish adding new book functionality
    When a form is submitted we capture the form data with javascript,
    we create a book object, add it to the myLibrary array and finally
    dispatch the displayBooksEvent, so we display all the books inside
    the myLibrary array again.

--- scripts/script.js
+++ scripts/script.js
@@ -68,12 +68,25 @@ document.dispatchEvent(displayBooksEvent);
 const modal = document.querySelector('.modal');
 const openModal = document.querySelector('.open-modal');
 const closeModal = document.querySelector('.close-modal');
+const modalForm = document.querySelector('.modal-form');
 
 openModal.addEventListener('click', () => {
     modal.showModal();
 })
 
 closeModal.addEventListener('click', () => {
+    modal.close();
+})
+
+modalForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = Object.fromEntries(new FormData(e.target).entries());
+    const bookObject = new Book(formData.title, !!formData.hasRead, formData.author, formData.pages);
+    addBookToLibrary(bookObject);
+    document.dispatchEvent(displayBooksEvent);
+
+    modalForm.reset();
     modal.close();
 })
 
